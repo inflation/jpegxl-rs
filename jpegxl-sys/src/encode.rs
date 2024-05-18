12710d61d3c4eed973b0be4de8322a7091626722
Inflation <2375962+inflation@users.noreply.github.com>
2024-05-18 12:44:23 +0800

    Add `JxlEncoderUseBoxes` for metadata encode
    - Add metadata test, test encoder.use_box

--- jpegxl-sys/src/encode.rs
+++ jpegxl-sys/src/encode.rs
@@ -1,19 +1,19 @@
 /*
-This file is part of jpegxl-sys.
-
-jpegxl-sys is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-jpegxl-sys is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with jpegxl-sys.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ * This file is part of jpegxl-rs.
+ *
+ * jpegxl-rs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jpegxl-rs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jpegxl-rs.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 use std::ffi::{c_char, c_void};
 
@@ -242,7 +242,7 @@ extern "C" {
 
     pub fn JxlEncoderAddBox(
         enc: *mut JxlEncoder,
-        box_type: JxlBoxType,
+        box_type: *const JxlBoxType,
         contents: *const u8,
         size: usize,
         compress_box: JxlBool,
